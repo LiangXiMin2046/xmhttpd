587069f7021da852e9f96ca2a0c51fb38f67de88
LiangXiMin2046 <1045886584@qq.com>
2021-01-09 16:50:20 +0800

    main

---
+++ httpd.cc
@@ -0,0 +1,40 @@
+//create daemon process and http server start
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+#include "HttpServer.h"
+
+int main(void)
+{
+
+	switch(fork())
+	{
+		case -1:
+			::exit(-1);
+		case 0:
+			break;
+		default:
+			::exit(0);
+	}
+
+	if(setsid() == -1)
+		::exit(-1);
+
+	umask(0);	
+
+	switch(fork())
+	{
+		case -1:
+			::exit(-1);
+		case 0:
+			break;
+		default:
+			::exit(0); 
+	}						
+
+	HttpServer server(80); //run on port 80 for http
+	server.start();	
+	return 0;
+}
